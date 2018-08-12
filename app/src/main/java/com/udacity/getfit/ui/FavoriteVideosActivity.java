4ab7a06394133f2c825d8b9cd89b006e3c82eedd
harshadpune <harshadpune@gmail.com>
2018-08-12 13:08:51 +0530

    GetFit : Capstone 2 : Firebase : Favorites video list implemented

--- app/src/main/java/com/udacity/getfit/ui/FavoriteVideosActivity.java
+++ app/src/main/java/com/udacity/getfit/ui/FavoriteVideosActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.WindowManager;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -14,6 +15,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.udacity.getfit.R;
 import com.udacity.getfit.utils.FavoritesCardsRecyclerAdapter;
+import com.udacity.getfit.utils.Utils;
 
 import java.util.ArrayList;
 
@@ -46,7 +48,7 @@ public class FavoriteVideosActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren()) {
                     Log.d("YoutbeActivity","--------"+dataSnapshot1.getKey());
-                    if(dataSnapshot1.getKey().equalsIgnoreCase("harshadpune")){
+                    if(dataSnapshot1.getKey().equalsIgnoreCase(Utils.getCurrentUserForDB(FirebaseAuth.getInstance().getCurrentUser().getEmail()))){
                         ArrayList<String> favoriteList = (ArrayList<String>) dataSnapshot1.getValue();
                         populateFavoriteData(favoriteList);
 
